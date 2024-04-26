7d5ce2aad7ec2cfbef0b676cade381e3d7276ab3
brai44 <braian.urba@gmail.com>
2024-04-26 18:18:17 -0400

    Feat metodo sabor de Snickers
    Para conseguir el sabor del Dulce

--- Tarea_01/src/main/java/org/example/Snickers.java
+++ Tarea_01/src/main/java/org/example/Snickers.java
@@ -1,4 +1,10 @@
 package org.example;
 
 public class Snickers extends Dulces{
+    public Snickers(int serie){
+        super(serie);
+    }
+    public String sabor(){
+        return "Snickers";
+    }
 }
