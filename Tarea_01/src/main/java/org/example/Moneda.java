c07bb2da5ed3484fee6593ef11e5107fe3496c93
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-04-26 16:49:31 -0400

    Feat getters/compareTo Moneda
    Se agregaron los getters para conseguir los valores de las subclases ademas de la implementacion del compareTo

--- Tarea_01/src/main/java/org/example/Moneda.java
+++ Tarea_01/src/main/java/org/example/Moneda.java
@@ -1,4 +1,14 @@
 package org.example;
 
-public class Moneda {
+abstract class Moneda implements Comparable<Moneda> {
+    public Moneda(){
+    }
+    public Moneda getSerie(){
+        return this;
+    }
+    public abstract int getValor();
+    @Override
+    public int compareTo(Moneda other) {
+        return this.getValor() - other.getValor();
+    }
 }
