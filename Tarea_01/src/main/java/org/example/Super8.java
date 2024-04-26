e602f5b9f92815fedafb8c119366c9597e0c4f20
brai44 <braian.urba@gmail.com>
2024-04-26 18:18:39 -0400

    Feat metodo sabor de Super8
    Para conseguir el sabor del Dulce

--- Tarea_01/src/main/java/org/example/Super8.java
+++ Tarea_01/src/main/java/org/example/Super8.java
@@ -1,4 +1,10 @@
 package org.example;
 
 public class Super8 extends Dulces{
+    public Super8(int serie){
+        super(serie);
+    }
+    public String sabor(){
+        return "Super8";
+    }
 }
