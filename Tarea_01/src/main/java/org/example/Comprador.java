22f6b1aca1c4782559b058d925467977eca2215c
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-04-26 16:53:25 -0400

    Feat Comprador/Vuelto/Producto en Comprador
    se agrego el llamado a expendedora para obtener la bebida
    se agrego la operacion que entrega el vuelto
    se agrego el valor que retorna el producto

--- Tarea_01/src/main/java/org/example/Comprador.java
+++ Tarea_01/src/main/java/org/example/Comprador.java
@@ -1,4 +1,22 @@
 package org.example;
 
 public class Comprador {
+    private String sonido=null;
+    private int vuelto = 0
+    public Comprador(Moneda m, Productos cualpro, Expendedora exp) throws Exception{
+        Producto aux = exp.ComprarProducto(m, cualpro);
+        if (aux != null) {
+            sonido = aux.sabor();
+        }
+        int canVuelt = exp.getmonVuSize();
+        for(int x = 0; x < canVuelt; x++){
+            vuelto += (exp.getVuelto()).getValor();
+        }
+    }
+    public int cuantoVuelto(){
+        return vuelto;
+    }
+    public String queProducto(){
+        return sonido;
+    }
 }
