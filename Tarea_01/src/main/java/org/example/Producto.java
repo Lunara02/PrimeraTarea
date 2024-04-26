818788a87e7ce2c3b8a58a8a8cd63b8314399c70
brai44 <braian.urba@gmail.com>
2024-04-26 17:28:54 -0400

    Feat Producto añade propiedad serie
    Se agrego getter de serie
    
    Se agrego metodo abstracto sabor para que cada producto lo implemente

--- Tarea_01/src/main/java/org/example/Producto.java
+++ Tarea_01/src/main/java/org/example/Producto.java
@@ -1,4 +1,12 @@
 package org.example;
 
 abstract public class Producto {
+    private int serie;
+    public Producto(int serie){
+        this.serie = serie;
+    }
+    public int getSerie(){
+        return serie;
+    }
+    public abstract String sabor();
 }
