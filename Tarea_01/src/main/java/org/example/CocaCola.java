c4611cc9d5a0a8b9f2381545a698d81a35644f84
brai44 <braian.urba@gmail.com>
2024-04-26 18:16:13 -0400

    Feat metodo sabor de CocaCola
    Para conseguir el sabor de la Bebida

--- Tarea_01/src/main/java/org/example/CocaCola.java
+++ Tarea_01/src/main/java/org/example/CocaCola.java
@@ -1,4 +1,10 @@
 package org.example;
 
 public class CocaCola extends Bebida{
+    public CocaCola(int serie){
+        super(serie);
+    }
+    public String sabor(){
+        return "CocaCola";
+    }
 }
