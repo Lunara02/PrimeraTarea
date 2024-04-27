c834dc71168e1838115198766b5777aa5c2ae851
brai44 <braian.urba@gmail.com>
2024-04-26 20:17:00 -0400

    Documentacion Bebida
    se agrego la documentacion de Bebida

--- Tarea_01/src/main/java/org/example/Bebida.java
+++ Tarea_01/src/main/java/org/example/Bebida.java
@@ -1,7 +1,16 @@
 package org.example;
 
+/**
+ * Clase abstracta Bebida que representa un tipo de producto, hereda la propiedad serie.
+ * Se usara para diferenciar los productos de tipo Bebida de los productos de tipo Dulces.
+ */
 abstract public class Bebida extends Producto{
+    /**
+     * Constructor de la clase Bebida.
+     * @param serie El numero de serie de la bebida.
+     */
     public Bebida(int serie){
         super(serie);
     }
+
 }
