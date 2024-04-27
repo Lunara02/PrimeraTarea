41b6469430b804838795d4a0e8d40fcc2c3014ce
brai44 <braian.urba@gmail.com>
2024-04-26 20:17:54 -0400

    Documentacion CocaCola
    se agrego la documentacion de CocaCola

--- Tarea_01/src/main/java/org/example/CocaCola.java
+++ Tarea_01/src/main/java/org/example/CocaCola.java
@@ -1,9 +1,22 @@
 package org.example;
 
+/**
+ * Clase CocaCola, representa un Producto de tipo Bebida.
+ * Implementa el metodo sabor (metodo abstracto de la clase Producto) del cual se obtiene su sabor.
+ */
 public class CocaCola extends Bebida{
+    /**
+     * Constructor de CocaCola.
+     * @param serie El numero de serie de la bebida CocaCola.
+     */
     public CocaCola(int serie){
         super(serie);
     }
+
+    /**
+     * Metodo que otorga el sabor del producto.
+     * @return El sabor de la bebida CocaCola.
+     */
     public String sabor(){
         return "CocaCola";
     }
