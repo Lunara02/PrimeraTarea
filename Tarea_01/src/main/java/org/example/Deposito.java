b4f8bb58cb4519465012d42916669545198b13dd
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-04-26 23:26:15 -0400

    Revert "Creacion constructor Deposito"
    This reverts commit 7cb40fd2a1e06b5301334a05a83f99771b669f43.

--- Tarea_01/src/main/java/org/example/Deposito.java
+++ Tarea_01/src/main/java/org/example/Deposito.java
@@ -1,7 +1,4 @@
 package org.example;
-import java.util.ArrayList;
 
 public class Deposito {
-    public Deposito(){
-    }
 }
--- Tarea_01/src/main/java/org/example/Expendedora.java
+++ Tarea_01/src/main/java/org/example/Expendedora.java
@@ -5,13 +5,6 @@ public class Expendedora {
     private Deposito<Dulces> snicker = new Deposito<>(), super8 = new Deposito<>();
     private Deposito<Moneda> monVu = new Deposito<>();
     public Expendedora(int numProductos) {
-        for (int i = 0; i < numProductos; i++) {
-            coca.addProducto(new CocaCola(i + 100));
-            sprite.addProducto(new Sprite(i + 200));
-            fanta.addProducto(new Fanta(i + 300));
-            snicker.addProducto(new Snickers(i + 400));
-            super8.addProducto(new Super8(i + 500));
-        }
     }
     public Producto ComprarProducto(){
     }
