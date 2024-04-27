21de1b9065358d1b13661a05c2abc51d1bba1217
Sebavegs2004 <sevega2023@inf.udec.cl>
2024-04-26 22:26:57 -0400

    Feat crear Depositos
    Se agregan lo depositos con los tipos correspondientes

--- Tarea_01/src/main/java/org/example/Expendedora.java
+++ Tarea_01/src/main/java/org/example/Expendedora.java
@@ -1,6 +1,9 @@
 package org.example;
 
 public class Expendedora {
+    private Deposito<Bebida> coca = new Deposito<>(), sprite = new Deposito<>(), fanta = new Deposito<>();
+    private Deposito<Dulces> snicker = new Deposito<>(), super8 = new Deposito<>();
+    private Deposito<Moneda> monVu = new Deposito<>();
     public Expendedora(int numProductos) {
     }
     public Producto ComprarProducto(){
