8bdd1019626d2874d0cd7b59d49f6a9790b7573e
Sebavegs2004 <sevega2023@inf.udec.cl>
2024-04-28 00:32:06 -0400

    Fix commit Documentacion Expendedora + Fix metodo ComprarProducto
    Se agrego mas documentacion y se cambio b por seleccion para mayor legibilidad

--- Tarea_01/src/main/java/org/example/Expendedora.java
+++ Tarea_01/src/main/java/org/example/Expendedora.java
@@ -1,11 +1,23 @@
 package org.example;
 
+/**
+ * La clase Expendedora representa una máquina expendedora que vende productos como bebidas o dulces.
+ * Esta vende los productos al comprador dependiendo de los parametros otorgados.
+ * Crea magicamente Productos que seran puestos en los depositos correspondientes.
+ * Finalmente retorna el vuelto y el producto seleccionado.
+ * @author Sebastian Vega Ignacio Varela
+ */
 public class Expendedora {
-    private Deposito<Bebida> coca = new Deposito<>(), sprite = new Deposito<>(), fanta = new Deposito<>();
-    private Deposito<Dulces> snicker = new Deposito<>(), super8 = new Deposito<>();
-    private Deposito<Moneda> monVu = new Deposito<>();
+    private Deposito<Bebida> coca = new Deposito<>(), sprite = new Deposito<>(), fanta = new Deposito<>(); //Deposito de Bebidas
+    private Deposito<Dulces> snicker = new Deposito<>(), super8 = new Deposito<>(); //Deposito de Dulces
+    private Deposito<Moneda> monVu = new Deposito<>(); //Deposito de Monedas
+    /**
+     * Constructor de la clase Expendedora.
+     * Rellena magicamente los depositos con el producto correspondiente
+     * @param numProductos El número de productos que se agregarán a la expendedora.
+     */
     public Expendedora(int numProductos) {
-        for (int i = 0; i < numProductos; i++) {
+        for (int i = 0; i < numProductos; i++) {  //Rellena magicamente los depositos
             coca.addProducto(new CocaCola(i + 100));
             sprite.addProducto(new Sprite(i + 200));
             fanta.addProducto(new Fanta(i + 300));
@@ -13,32 +25,41 @@ public class Expendedora {
             super8.addProducto(new Super8(i + 500));
         }
     }
+    /**
+     * Método para comprar un producto de la expendedora.
+     * @param m La moneda utilizada para la compra.
+     * @param cual El tipo de producto que se desea comprar.
+     * @return El producto comprado.
+     * @throws PagoIncorrectoException Si la moneda es nula.
+     * @throws PagoInsuficienteException Si el valor de la moneda no es suficiente para comprar el producto.
+     * @throws NoHayProductoException Si no hay existencias del producto seleccionado.
+     */
     public Producto ComprarProducto(Moneda m, Productos cual)throws Exception{
-        if(m != null){
-            if (m.getValor() >= cual.RetValor()) {
-                Deposito<? extends Producto> b = null;
+        if(m != null){ //Caso PagoIncorrecto
+            if (m.getValor() >= cual.RetValor()) { //Caso PagoInsuficiente
+                Deposito<? extends Producto> seleccion = null; //Selector de deposito para referenciar
                 switch (cual) {
                     case COCACOLA:
-                        b = coca;
+                        seleccion = coca;
                         break;
                     case SPRITE:
-                        b = sprite;
+                        seleccion = sprite;
                         break;
                     case FANTA:
-                        b = fanta;
+                        seleccion = fanta;
                         break;
                     case SNICKERS:
-                        b = snicker;
+                        seleccion = snicker;
                         break;
                     case SUPER8:
-                        b = super8;
+                        seleccion = super8;
                         break;
                 }
-                if (b.getSize() != 0) {
+                if (seleccion.getSize() != 0) { //Caso NoHayProductos
                     for (int i = 0; i < (m.getValor() - cual.RetValor()) / 100; i++) {
                         monVu.addProducto(new Moneda100());
                     }
-                    return b.getProducto();
+                    return seleccion.getProducto();
                 }
                 monVu.addProducto(m);
                 throw new NoHayProductoException();
@@ -48,9 +69,17 @@ public class Expendedora {
         }
         throw new PagoIncorrectoException();
     }
+    /**
+     * Método para obtener el vuelto acumulado en la máquina expendedora.
+     * @return La moneda que representa el vuelto.
+     */
     public Moneda getVuelto(){
         return monVu.getProducto();
     }
+    /**
+     * Método para obtener el tamaño del depósito de monedas en la expendedora.
+     * @return El tamaño del depósito de monedas.
+     */
     public int getmonVuSize(){
         return monVu.getSize();
     }
