1654f1bb7a38061e1a9287b5b93bfd2f333a05b4
Sebavegs2004 <sevega2023@inf.udec.cl>
2024-04-28 00:12:28 -0400

    Revert "Documentacion Expendedora"
    This reverts commit 25f56a4ad73055475708668323940de178c498a2.

--- Tarea_01/src/main/java/org/example/Expendedora.java
+++ Tarea_01/src/main/java/org/example/Expendedora.java
@@ -1,21 +1,9 @@
 package org.example;
 
-/**
- * La clase Expendedora representa una máquina expendedora que vende productos como bebidas o dulces.
- * Esta vende los productos al comprador dependiendo de los parametros otorgados.
- * Crea magicamente Productos que seran puestos en los depositos correspondientes.
- * Finalmente retorna el vuelto y el producto seleccionado.
- * @author Sebastian Vega Ignacio Varela
- */
 public class Expendedora {
     private Deposito<Bebida> coca = new Deposito<>(), sprite = new Deposito<>(), fanta = new Deposito<>();
     private Deposito<Dulces> snicker = new Deposito<>(), super8 = new Deposito<>();
-    private Deposito<Moneda> monVu = new Deposito<>(); // Depósito para almacenar monedas
-
-    /**
-     * Constructor de la clase Expendedora.
-     * @param numProductos El número de productos que se agregarán a la expendedora.
-     */
+    private Deposito<Moneda> monVu = new Deposito<>();
     public Expendedora(int numProductos) {
         for (int i = 0; i < numProductos; i++) {
             coca.addProducto(new CocaCola(i + 100));
@@ -25,36 +13,24 @@ public class Expendedora {
             super8.addProducto(new Super8(i + 500));
         }
     }
-
-    /**
-     * Método para comprar un producto de la expendedora.
-     * @param m La moneda utilizada para la compra.
-     * @param cual El tipo de producto que se desea comprar.
-     * @return El producto comprado.
-     * @throws PagoIncorrectoException Si la moneda es nula.
-     * @throws PagoInsuficienteException Si el valor de la moneda no es suficiente para comprar el producto.
-     * @throws NoHayProductoException Si no hay existencias del producto seleccionado.
-     */
-    public Producto ComprarProducto(Moneda m, Productos cual) throws Exception {
+    public Producto ComprarProducto(Moneda m, Productos cual)throws Exception{
         if(m != null){
             if (m.getValor() >= cual.RetValor()) {
                 Deposito<? extends Producto> b = null;
-                switch (cual) {
-                    case COCACOLA:
-                        b = coca;
-                        break;
-                    case SPRITE:
-                        b = sprite;
-                        break;
-                    case FANTA:
-                        b = fanta;
-                        break;
-                    case SNICKERS:
-                        b = snicker;
-                        break;
-                    case SUPER8:
-                        b = super8;
-                        break;
+                if (cual.ordinal() == 0) {
+                    b = coca;
+                }
+                if (cual.ordinal() == 1) {
+                    b = sprite;
+                }
+                if (cual.ordinal() == 2) {
+                    b = fanta;
+                }
+                if (cual.ordinal() == 3) {
+                    b = snicker;
+                }
+                if (cual.ordinal() == 4) {
+                    b = super8;
                 }
                 if (b.getSize() != 0) {
                     for (int i = 0; i < (m.getValor() - cual.RetValor()) / 100; i++) {
@@ -70,19 +46,9 @@ public class Expendedora {
         }
         throw new PagoIncorrectoException();
     }
-
-    /**
-     * Método para obtener el vuelto acumulado en la máquina expendedora.
-     * @return La moneda que representa el vuelto.
-     */
     public Moneda getVuelto(){
         return monVu.getProducto();
     }
-
-    /**
-     * Método para obtener el tamaño del depósito de monedas en la expendedora.
-     * @return El tamaño del depósito de monedas.
-     */
     public int getmonVuSize(){
         return monVu.getSize();
     }
