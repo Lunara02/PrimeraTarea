f1ce6986945244c167428313c5f1e055ecd1c45f
Sebavegs2004 <sevega2023@inf.udec.cl>
2024-04-28 01:40:43 -0400

    Documentacion Productos

--- Tarea_01/src/main/java/org/example/Productos.java
+++ Tarea_01/src/main/java/org/example/Productos.java
@@ -1,15 +1,28 @@
 package org.example;
 
+/**
+ * Enumeración que representa diferentes productos y sus precios.
+ * Se asignan los precios que se van a utilizar (se puede cambiar).
+ * @author Sebastian Ignacio Vega Varela
+ */
 public enum Productos {
-    COCACOLA(900),
-    SPRITE(800),
-    FANTA(600),
-    SNICKERS(1000),
-    SUPER8(300);
-    private int precio;
-    private Productos(int precio){
+    COCACOLA(900),  // Precio de CocaCola
+    SPRITE(800),    // Precio de Sprite
+    FANTA(600),     // Precio de Fanta
+    SNICKERS(1000), // Precio de Snickers
+    SUPER8(300);    // Precio de Super8
+    private int precio; // Precio del producto
+    /**
+     * Constructor privado de enum Productos.
+     * @param precio El precio del producto.
+     */
+    private Productos(int precio) {
         this.precio = precio;
     }
+    /**
+     * Método getter para retornar el precio del producto.
+     * @return El precio del producto.
+     */
     public int RetValor() {
         return precio;
     }
