e328459212e139f72e06015cf055ecbbb8cec265
blokkxz_ <166780700+Lunara02@users.noreply.github.com>
2024-04-28 15:48:48 -0400

    Documentacion NoHayProducto
    se agrego la documentacion a la excepcion no hay producto y a su constructor

--- Tarea_01/src/main/java/org/example/NoHayProductoException.java
+++ Tarea_01/src/main/java/org/example/NoHayProductoException.java
@@ -1,6 +1,14 @@
 package org.example;
-
+/**
+ * Esta clase representa una excepción que se lanza cuando no hay productos disponibles.
+ * Extiende la clase abstracta Exception y proporciona una implementación personalizada del constructor.
+ * @author Cristobal
+ */
 public class NoHayProductoException extends Exception{
+    /**
+     * Constructor que crea una instancia de NoHayProductoException.
+     * Utiliza el constructor de la superclase Exception para enviar el mensaje.
+     */
     public NoHayProductoException(){
         super("[NO HAY PRODUCTOS]");
     }
