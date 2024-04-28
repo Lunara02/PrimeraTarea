36e55dc30d9c274494c86483cf9d18d26aaf2f4b
Sebavegs2004 <sevega2023@inf.udec.cl>
2024-04-27 22:30:45 -0400

    Creacion Objetos main
    Creamos los objetos que vamos a usar como parametros

--- Tarea_01/src/main/java/org/example/Main.java
+++ Tarea_01/src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+        Expendedora exp = new Expendedora(1);
+        Moneda1000 m = new Moneda1000();
+        Moneda100 mon = new Moneda100();
     }
 }
